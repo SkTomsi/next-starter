ec0d6ea6392cb2f3f01d757d28348782a81c671f
sktomsi <tomcy48@gmail.com>
2024-09-18 14:05:48 +0530

    add mobile responsiveness for landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,22 @@ import { IconFlame } from "@tabler/icons-react";
 
 export default function Home() {
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-10 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="flex h-[500px] flex-col items-center justify-center gap-10 font-[family-name:var(--font-geist-sans)] sm:p-20 md:h-full">
       <div className="flex flex-col items-center justify-center gap-4 text-center">
-        <div className="flex items-center gap-x-2 text-5xl font-bold tracking-tighter">
+        <div className="flex flex-col items-center gap-y-5 text-4xl font-bold tracking-tighter md:flex-row md:gap-x-2 md:text-5xl">
           <IconFlame size={56} fill="#ba84fe" color="#7c3aed" />
           Fire-stack: A light-weight Next.js starter
         </div>
-        <p className="text-balance font-[family-name:var(--font-geist-mono)] text-sm">
+        <p className="text-balance font-[family-name:var(--font-geist-mono)] text-xs md:text-sm">
           A lightweight and scalable Next.js starter pack with all the necessary
           rules and formatting? Bro, that's straight-up fire! Perfect for
           keeping it sleek and lit, with all the essentials to make it pop. 🔥💻
         </p>
       </div>
-      <Button className="w-fit">Get Started</Button>
-      <div className="absolute inset-0 -z-[9] bg-[url(/grains.svg)] bg-cover bg-center mix-blend-overlay"></div>
+      <Button className="w-fit" size={"lg"}>
+        Get Started
+      </Button>
+      <div className="absolute inset-0 -z-[9] bg-[url(/grains.svg)] bg-cover bg-center mix-blend-soft-light"></div>
       <div className="absolute inset-0 -z-10 bg-[url(/gradient-light.svg)] bg-cover bg-center [mask-image:linear-gradient(360deg,white,rgba(255,255,255,0))]"></div>
     </div>
   );
